43c731b9fbbbc31be17d309739649ac45b87b6cf
frtem2008 <66516950+frtem2008@users.noreply.github.com>
2022-09-27 20:19:58 +0300

    Update A.C

--- A.C
+++ A.C
@@ -1,19 +1,19 @@
-//#include <stdio.h>
-//
-////extern int value;
-////int value = 4;
-//
-//void a( void )
-//{
-//  printf("%i", value);
-//}
-//
-//void b( void );
-//
-//void main( void )
-//{
-//  	a();
-//    printf("\n");
-//    b();
-//    getchar();
-//}
+#include <stdio.h>
+
+extern int value;
+int value = 4;
+
+void a( void )
+{
+  printf("%i", value);
+}
+
+void b( void );
+
+void main( void )
+{
+  	a();
+    printf("\n");
+    b();
+    getchar();
+}
